4333b334c5e47bc970f7e370f3e847813082c6e9
Denis Doan <dedoin08@yahoo.de>
2015-09-30 21:41:36 +0100

    Chat with 1 user

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
-app.set('view engine', 'ejs')
-app.set('views', './views')
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
